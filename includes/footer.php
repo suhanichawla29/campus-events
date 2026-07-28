3f50313d29a5e99ee5a1d5df26238468a3353d97
Suhani Chawla <suhanichawla47@gmail.com>
2026-07-28 21:45:07 +0530

    Update footer text to include author name

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,6 @@
 </div>
 <footer class="footer">
-    <p>&copy; 2026 Campus Event Management System. All rights reserved.</p>
+    <p>&copy; 2026 Campus Event Management System.Suhani Chawla- All rights reserved.</p>
 </footer>
 <script src="assets/js/script.js"></script>
 </body>
